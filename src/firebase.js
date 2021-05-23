a5c0e3c0589712277456be16a2783fde49cb6ee5
Rokas <rookas.rudzenskas@gmail.com>
2021-05-23 21:51:05 +0300

    Rokas

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBWHzka9XxdZhOo-aAaflJZvFoH-mAwAy8",
+    authDomain: "rookas-slack-app.firebaseapp.com",
+    projectId: "rookas-slack-app",
+    storageBucket: "rookas-slack-app.appspot.com",
+    messagingSenderId: "82316297614",
+    appId: "1:82316297614:web:87500c63f18accd40090df",
+    measurementId: "G-X494T622QH"
+};
+

a9c99072791eee231a83a5a2264a36b53b275576
Rokas <rookas.rudzenskas@gmail.com>
2021-05-24 08:28:37 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import Header from "./components/Header";
 import styled from "styled-components";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import firebase from "firebase";
+import Login from "./components/Login";
 
 function App() {
+    const [user, loading] = useAuthState(auth);
   return (
     <div className="app">
       <Router>
+          {!user ? (
+              // <h1>Hello</h1>
+              <Login />
+          ) : (
           <>
               <Header />
                   <Switch>
@@ -22,6 +31,7 @@ function App() {
                       </AppBody>
                   </Switch>
              </>
+          )}
       </Router>
     </div>
   );

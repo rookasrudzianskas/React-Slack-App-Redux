adcb60e29164c119f1f90821080ea72380509e67
Rokas <rookas.rudzenskas@gmail.com>
2021-05-24 09:16:38 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
 import firebase from "firebase";
 import Login from "./components/Login";
+import Spinner from "react-spinkit";
 
 function App() {
     const [user, loading] = useAuthState(auth);
@@ -17,7 +18,7 @@ function App() {
             <AppLoading>
                 <AppLoadingContents>
                     <img src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png" alt=""/>
-
+                        <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none"  />
                 </AppLoadingContents>
             </AppLoading>
         )
@@ -55,10 +56,25 @@ const AppBody = styled.div`
 `;
 
 const AppLoading = styled.div`
-  
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
 `;
 
 const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   
+  > img {
+    object-fit: contain;
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
 `;
 
